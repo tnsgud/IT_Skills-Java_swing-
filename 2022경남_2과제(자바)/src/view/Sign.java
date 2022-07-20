8069ca2904e9f2f361c88877a3877377717bb92f
tnsgud <qkrtnsgud0229@gmail.com>
2022-07-20 11:35:19 +0900

    2022경남_2과제(자바): 40일 공개과제가 나와서 여기까지

--- 2022경남_2과제(자바)/src/view/Sign.java
+++ 2022경남_2과제(자바)/src/view/Sign.java
@@ -1,9 +1,17 @@
 package view;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+
 public class Sign extends BaseFrame {
 	public Sign() {
 		super("회원가입", 500, 500);
-		
+
+		add(n = new JPanel(), "North");
+		add(c = new JPanel());
+		add(s = new JPanel(new FlowLayout(1, 50, 0)), "South");
+
 		setVisible(true);
 	}
 }
