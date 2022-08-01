702fbbda314ded09b0f3fb8c0012d71699832c97
tnsgud <qkrtnsgud0229@gmail.com>
2022-08-01 11:18:46 +0900

    2022대전_2과제(자바)_PSH_1 :  KnockCode.java 완성

---
+++ 2022대전_2과제(자바)_PSH_1/src/view/KnockCode.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
+public class KnockCode extends BaseFrame {
+	String num = "";
+
+	public KnockCode(JTextField txt) {
+		super("노크코드", 500, 500);
+
+		add(c = new JPanel(new GridLayout(0, 3)));
+		add(s = new JPanel(), "South");
+
+		for (int i = 0; i < 9; i++) {
+			var l = lbl("●", 0, 30);
+
+			l.setName(i + 1 + "");
+			l.setBorder(new LineBorder(Color.LIGHT_GRAY));
+
+			l.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent e) {
+					num += l.getName();
+					System.out.println(num);
+				}
+			});
+
+			c.add(l);
+		}
+
+		s.add(btn("확인", a -> {
+			txt.setText(num);
+			dispose();
+		}));
+
+		setVisible(true);
+	}
+}
